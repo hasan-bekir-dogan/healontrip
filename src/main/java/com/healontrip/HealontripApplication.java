b324d16079fa5d8f3445306cd228b678f2db471a
hasanbekir97 <hasan.bekir.dogan@hotmail.com>
2023-04-04 01:37:14 +0300

    Base Structure Created

---
+++ src/main/java/com/healontrip/HealontripApplication.java
@@ -0,0 +1,18 @@
+package com.healontrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
+//@SpringBootApplication
+public class HealontripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealontripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healontrip/HealontripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healontrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealontripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
