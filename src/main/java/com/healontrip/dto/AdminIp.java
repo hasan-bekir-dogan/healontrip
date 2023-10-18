024db90e403a4c27271fe53597b46f1d50ea992a
hasanbekir97 <hasan.bekir.dogan@hotmail.com>
2023-10-19 01:59:17 +0300

    AdminIp adjusted

--- src/main/java/com/healontrip/dto/AdminIp.java
+++ src/main/java/com/healontrip/dto/AdminIp.java
@@ -6,9 +6,9 @@ public enum AdminIp {
             "212.252.142.144", // Adil - İzmir Ev interneti
             "88.241.79.94", // Adil - Aydın Ev interneti
             "24.133.52.76", // Okan
-            "192.168.1.127",
+            //"192.168.1.127",
             "46.154.70.59", // Adil - Telefon
-            "5.47.76.62" // Bekir - Telefon
+            "5.47.23.148", // Bekir - Telefon
     });
 
     private final String[] ip;
